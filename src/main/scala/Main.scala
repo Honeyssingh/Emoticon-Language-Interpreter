ca290a5892b81475c5b48c0692b739d939191a6c
Bhoopender Singh <bhsi5988@colorado.edu>
2024-08-07 17:17:59 -0600

    Updated with more cases and better oragnized.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,83 +1,84 @@
-// Sealed trait for all expressions
+// Define the abstract syntax for expressions and values
 sealed trait Expr
-
-// Sealed trait for values, which are a subset of expressions
 sealed trait Value extends Expr
 
-// Case classes representing different emoticon values
-case class Cry() extends Value
-case class Happy() extends Value
-case class VeryHappy() extends Value
-case class Sleepy() extends Value
-case class Stun() extends Value
-case class ManyVals(values: List[Value]) extends Value // List of multiple values
+// Different types of expressions
+case class ManyExprs(exprs: List[Expr]) extends Expr // A list of expressions
+case class Plus(left: Expr, right: Expr) extends Expr // Addition of two expressions
+case class Not(expr: Expr) extends Expr // Negation of an expression
+case class Count(left: Expr, right: Expr) extends Expr // Count operation between two expressions
 
-// Case classes representing different types of expressions
-case class ManyExprs(exprs: List[Expr]) extends Expr // List of multiple expressions
-case class Plus(left: Expr, right: Expr) extends Expr // Addition operation
-case class Not(expr: Expr) extends Expr // Negation operation
-case class Count(left: Expr, right: Expr) extends Expr // Count operation (not implemented)
+// Different types of values
+case object Cry extends Value // Represents a crying emoticon
+case object Happy extends Value // Represents a happy emoticon
+case object VeryHappy extends Value // Represents a very happy emoticon
+case object Sleepy extends Value // Represents a sleepy emoticon
+case object Stun extends Value // Represents a stunned emoticon
+case class ManyVals(values: List[Value]) extends Value // A list of values
 
-// Object containing the interpreter logic
 object Interpreter {
-  // Evaluation function for expressions
   def eval(expr: Expr): Value = expr match {
-    // If the expression is already a value, return it as is
+    // Base case: if the expression is already a value, return it
     case v: Value => v
 
-    // Evaluation for a list of expressions
+    // Evaluate a list of expressions
     case ManyExprs(exprs) =>
-      val values = exprs.map(eval) // Evaluate each expression in the list
-      if (values.contains(ERROR)) ERROR // If any evaluation results in an error, return error
-      else ManyVals(values) // Otherwise, return the list of evaluated values
+      val evaluated = exprs.map(eval) // Evaluate each expression in the list
+      if (evaluated.contains(ERROR)) ERROR // If any expression evaluates to ERROR, return ERROR
+      else ManyVals(evaluated.collect { case v: Value => v }) // Otherwise, collect the evaluated values into a ManyVals
 
-    // Evaluation for the addition operation
-    case Plus(e1, e2) =>
-      (eval(e1), eval(e2)) match {
-        case (VeryHappy(), _) => VeryHappy() // If the left operand is VeryHappy, result is VeryHappy
-        case (_, VeryHappy()) => VeryHappy() // If the right operand is VeryHappy, result is VeryHappy
-        case (Cry(), v2) => v2 // If the left operand is Cry, result is the right operand
-        case (v1, Cry()) if v1 == Happy() || v1 == Stun() => Cry() // If the right operand is Cry and the left is Happy or Stun, result is Cry
-        case (v1, v2) if v1 != ERROR && v2 != VeryHappy() && v2 != Cry() => v1 // If neither operand is an error, result is the left operand
-        case (ManyVals(values), v2) =>
-          // Evaluate addition for each value in the list with the right operand
-          val newValues = values.map(v => eval(Plus(v, v2)))
-          if (newValues.contains(ERROR)) ERROR // If any result is an error, return error
-          else ManyVals(newValues) // Otherwise, return the list of new values
-        case (v1, v2) =>
-          // If either operand is an error, return error
-          if (v1 == ERROR || v2 == ERROR) ERROR
-          else v1
+    // Evaluate the addition of two expressions
+    case Plus(left, right) =>
+      (eval(left), eval(right)) match {
+        // If the left expression is a list of values, add the right value to each one
+        case (ManyVals(values), r) =>
+          val results = values.map(v => eval(Plus(v, r)))
+          if (results.contains(ERROR)) ERROR // If any addition results in ERROR, return ERROR
+          else ManyVals(results.collect { case v: Value => v }) // Otherwise, collect the results into a ManyVals
+
+        // If either expression is VeryHappy, the result is VeryHappy
+        case (VeryHappy, _) => VeryHappy
+        case (_, VeryHappy) => VeryHappy
+        // If the left expression is Cry, return the right value
+        case (Cry, r) => r
+        // If the right expression is Cry and the left is Happy or Stun, return Cry
+        case (v, Cry) if v == Happy || v == Stun => Cry
+        // If both expressions are neither ERROR nor special cases, return the left value
+        case (v1, v2) if v1 != ERROR && v2 != VeryHappy && v2 != Cry => v1
+        // If none of the above cases match, return ERROR
+        case _ => ERROR
       }
 
-    // Evaluation for the negation operation
-    case Not(e1) =>
-      eval(e1) match {
-        case Stun() => Sleepy() // If operand is Stun, result is Sleepy
-        case Sleepy() => Stun() // If operand is Sleepy, result is Stun
-        case Happy() | VeryHappy() => Cry() // If operand is Happy or VeryHappy, result is Cry
-        case Cry() => VeryHappy() // If operand is Cry, result is VeryHappy
-        case ManyVals(values) =>
-          // If operand is a list of values, reduce the list using addition and apply negation to the result
-          if (values.length >= 2) {
-            val reducedValue = values.reduce((v1, v2) => eval(Plus(v1, v2)))
-            eval(Not(reducedValue))
-          } else ERROR // If less than 2 values, return error
-        case other => other match {
-          // Apply specific negations to individual values
-          case Happy() => Cry()
-          case VeryHappy() => Cry()
-          case Cry() => VeryHappy()
-          case Sleepy() => Stun()
-          case Stun() => Sleepy()
-          case _ => ERROR // If unknown value, return error
-        }
+    // Evaluate the negation of an expression
+    case Not(expr) =>
+      eval(expr) match {
+        // If the expression is Stun, return Sleepy
+        case Stun => Sleepy
+        // If the expression is Sleepy, return Stun
+        case Sleepy => Stun
+        // If the expression is Happy or VeryHappy, return Cry
+        case Happy | VeryHappy => Cry
+        // If the expression is Cry, return VeryHappy
+        case Cry => VeryHappy
+        // If the expression is a list of values, reduce them using addition
+            case ManyVals(values) =>
+              if (values.isEmpty) ERROR // Added this line to handle empty list case
+              else values.reduceLeft((acc, v) => eval(Plus(acc, v)))
+        // If none of the above cases match, return ERROR
+        case _ => ERROR
       }
 
-    // Count operation is not implemented, always returns error
-    case Count(e1, e2) => ERROR
+    // Evaluate the count of occurrences of the right value in the left list of values
+    case Count(left, right) =>
+      (eval(left), eval(right)) match {
+        // If the left expression is a list of values, filter them by the right value
+        case (ManyVals(values), r) => ManyVals(values.filter(_ == r))
+        // If the right expression is a list of values, filter them by the left value
+        case (v, ManyVals(values)) => ManyVals(values.filter(_ == v))
+        // If none of the above cases match, return ERROR
+        case _ => ERROR
+      }
   }
 
-  // Define ERROR as Cry
-  val ERROR: Value = Cry()
+  val ERROR: Value = Cry // Define the ERROR value as Cry
 }
--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -3,122 +3,142 @@ import munit.FunSuite
 class MySuite extends FunSuite {
 
   test("eval-value") {
-    val obtained = Interpreter.eval(Happy())
-    val expected = Happy()
+    val obtained = Interpreter.eval(Happy)
+    val expected = Happy
     assertEquals(obtained, expected)
   }
 
   test("eval-many-exprs") {
-    val obtained = Interpreter.eval(ManyExprs(List(Happy(), VeryHappy())))
-    val expected = ManyVals(List(Happy(), VeryHappy()))
+    val obtained = Interpreter.eval(ManyExprs(List(Happy, VeryHappy)))
+    val expected = ManyVals(List(Happy, VeryHappy))
     assertEquals(obtained, expected)
   }
 
   test("eval-many-exprs-error") {
-    val obtained = Interpreter.eval(ManyExprs(List(Happy(), Interpreter.ERROR)))
+    val obtained = Interpreter.eval(ManyExprs(List(Happy, Interpreter.ERROR)))
     val expected = Interpreter.ERROR
     assertEquals(obtained, expected)
   }
 
   test("eval-plus-many-values") {
-    val obtained = Interpreter.eval(Plus(ManyVals(List(Happy(), VeryHappy())), Happy()))
-    val expected = ManyVals(List(Happy(), VeryHappy()))
+    val obtained = Interpreter.eval(Plus(ManyVals(List(Happy, VeryHappy)), Happy))
+    val expected = ManyVals(List(Happy, VeryHappy))
     assertEquals(obtained, expected)
   }
-//Not passing
+
   test("eval-plus-many-values-error") {
-    val obtained = Interpreter.eval(Plus(ManyVals(List(Happy(), Interpreter.ERROR)), Happy()))
+    val obtained = Interpreter.eval(Plus(ManyVals(List(Happy, Interpreter.ERROR)), Happy))
     val expected = Interpreter.ERROR
     assertEquals(obtained, expected)
   }
 
   test("eval-stay-uwu") {
-    val obtained = Interpreter.eval(Plus(VeryHappy(), Happy()))
-    val expected = VeryHappy()
+    val obtained = Interpreter.eval(Plus(VeryHappy, Happy))
+    val expected = VeryHappy
     assertEquals(obtained, expected)
   }
 
   test("eval-become-uwu") {
-    val obtained = Interpreter.eval(Plus(Happy(), VeryHappy()))
-    val expected = VeryHappy()
+    val obtained = Interpreter.eval(Plus(Happy, VeryHappy))
+    val expected = VeryHappy
     assertEquals(obtained, expected)
   }
 
   test("eval-move-on") {
-    val obtained = Interpreter.eval(Plus(Cry(), Happy()))
-    val expected = Happy()
+    val obtained = Interpreter.eval(Plus(Cry, Happy))
+    val expected = Happy
     assertEquals(obtained, expected)
   }
 
   test("eval-hard-day") {
-    val obtained = Interpreter.eval(Plus(Happy(), Cry()))
-    val expected = Cry()
+    val obtained = Interpreter.eval(Plus(Happy, Cry))
+    val expected = Cry
     assertEquals(obtained, expected)
   }
 
   test("eval-meh") {
-    val obtained = Interpreter.eval(Plus(Happy(), Stun()))
-    val expected = Happy()
+    val obtained = Interpreter.eval(Plus(Happy, Stun))
+    val expected = Happy
     assertEquals(obtained, expected)
   }
 
   test("eval-not-stun") {
-    val obtained = Interpreter.eval(Not(Stun()))
-    val expected = Sleepy()
+    val obtained = Interpreter.eval(Not(Stun))
+    val expected = Sleepy
     assertEquals(obtained, expected)
   }
 
   test("eval-not-sleepy") {
-    val obtained = Interpreter.eval(Not(Sleepy()))
-    val expected = Stun()
+    val obtained = Interpreter.eval(Not(Sleepy))
+    val expected = Stun
     assertEquals(obtained, expected)
   }
 
   test("eval-not-(very)-happy") {
-    val obtained = Interpreter.eval(Not(Happy()))
-    val expected = Cry()
+    val obtained = Interpreter.eval(Not(Happy))
+    val expected = Cry
     assertEquals(obtained, expected)
   }
 
   test("eval-not-cry") {
-    val obtained = Interpreter.eval(Not(Cry()))
-    val expected = VeryHappy()
+    val obtained = Interpreter.eval(Not(Cry))
+    val expected = VeryHappy
     assertEquals(obtained, expected)
   }
 
   test("eval-not-many-values") {
-    val obtained = Interpreter.eval(Not(ManyVals(List(Happy(), VeryHappy()))))
-    val expected = Cry()
+    val obtained = Interpreter.eval(Not(ManyVals(List(Happy, VeryHappy))))
+    val expected = Cry
     assertEquals(obtained, expected)
   }
 
   test("integration-test-1") {
-    val obtained = Interpreter.eval(Plus(ManyVals(List(Happy(), VeryHappy())), Not(ManyVals(List(Cry(), Stun())))))
-    val expected = ManyVals(List(Happy(), VeryHappy()))
+    val obtained = Interpreter.eval(Plus(ManyVals(List(Happy, VeryHappy)), Not(ManyVals(List(Cry, Stun)))))
+    val expected = ManyVals(List(Happy, VeryHappy))
     assertEquals(obtained, expected)
   }
-//Not passing
+
   test("integration-test-2") {
-    val obtained = Interpreter.eval(Plus(Happy(), Not(ManyVals(List(Happy(), Cry())))))
-    val expected = Cry()
+    val obtained = Interpreter.eval(Plus(Happy, Not(ManyVals(List(Happy, Cry)))))
+    val expected = Cry
     assertEquals(obtained, expected)
   }
-//Not passing
+
   test("integration-test-3") {
-    val obtained = Interpreter.eval(Not(Plus(ManyVals(List(Happy(), Cry())), VeryHappy())))
-    val expected = VeryHappy()
+    val obtained = Interpreter.eval(Not(Plus(ManyVals(List(Happy, Cry)), VeryHappy)))
+    val expected = VeryHappy
     assertEquals(obtained, expected)
   }
-//Not passing
+
   test("integration-test-4") {
-    val obtained = Interpreter.eval(Not(Plus(Cry(), VeryHappy())))
-    val expected = VeryHappy()
+    val obtained = Interpreter.eval(Not(Plus(Cry, VeryHappy)))
+    val expected = VeryHappy
     assertEquals(obtained, expected)
   }
 
   test("integration-test-5") {
-    val obtained = Interpreter.eval(Plus(Not(Happy()), VeryHappy()))
-    val expected = VeryHappy()
+    val obtained = Interpreter.eval(Plus(Not(Happy), VeryHappy))
+    val expected = VeryHappy
+    assertEquals(obtained, expected)
+  }
+
+  test("empty-many-exprs") {
+    val obtained = Interpreter.eval(ManyExprs(Nil))
+    val expected = ManyVals(Nil)
+    assertEquals(obtained, expected)
+  }
+
+  test("nested-expressions") {
+    val expr = Plus(Not(Plus(Happy, Cry)), ManyVals(List(VeryHappy, Stun)))
+    val obtained = Interpreter.eval(expr)
+    val expected = ManyVals(List(Cry, Stun))
+    assertEquals(obtained, expected)
+  }
+
+  test("combining-multiple-operations") {
+    val expr = Not(Plus(Count(ManyVals(List(Happy, Cry, Happy)), Not(Sleepy)), VeryHappy))
+    val obtained = Interpreter.eval(expr)
+    val expected = Cry
     assertEquals(obtained, expected)
   }
 }
